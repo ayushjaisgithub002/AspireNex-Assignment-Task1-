391a073667857bc36e8ab89119409a47e2406504
Ayush Jaiswal <135218732+ayushjaisgithub002@users.noreply.github.com>
2024-07-08 01:40:05 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// const form=document.querySelector('form');
+
+// function sendEmail() {
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "ayushjais004@gmail.com",
+//         Password : "BC0F9AB2DDF146E34C39391563F70C0C981A",
+//         To : 'them@website.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
